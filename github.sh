329ace6a08820b95f72e96abf36fde96e8352814
执迷不悟 <50208709+1078503@users.noreply.github.com>
2020-02-14 21:27:55 +0800

    Initial blog repo

--- github.sh
+++ github.sh
@@ -1,5 +1,6 @@
+hugo --theme=Mainroad --baseUrl="https://dtz9.net/"
 git init
+git add --all
+git commit -m "Initial blog repo"
 git remote add origin https://github.com/1078503/blog.git
-git add -A
-git commit -m "first commit"
-git push -u origin master
+git push origin master
