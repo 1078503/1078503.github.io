1b0c994a8d2ac4ba9cbe3796b5d81a5739eb1d01
g1078503 <1078503@gmail.com>
2020-02-20 07:17:05 +0800

    Initial blog repo

--- richang/github.sh
+++ richang/github.sh
@@ -1,18 +1,6 @@
-hugo --baseUrl="https://dtz9.net/"
+hugo
 git init
 git add --all
 git commit -m "Initial blog repo"
 git remote add origin https://github.com/1078503/blog.git
 git push origin master
-
-git pull origin master
-
-publishDir = "docs"
-
-git rm -r --cached "themes" 
-git commit -m "更新themes"
-git push -u origin master
-
-git clone https://github.com/zzossig/hugo-theme-zzo.git
-
-tar -zxvf ./hugo_0.60.0_Linux-64bit.tar.gz
