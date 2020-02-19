c72ebf581c425207667d343b7f92239ec775b8d1
g1078503 <1078503@gmail.com>
2020-02-20 03:26:30 +0800

    Initial blog repo

--- richang/github.sh
+++ richang/github.sh
@@ -1,4 +1,4 @@
-hugo --theme=Mainroad --baseUrl="https://dtz9.net/"
+hugo --baseUrl="https://dtz9.net/"
 git init
 git add --all
 git commit -m "Initial blog repo"
@@ -6,3 +6,9 @@ git remote add origin https://github.com/1078503/blog.git
 git push origin master
 
 publishDir = "docs"
+
+git rm -r --cached "themes" 
+git commit -m "更新themes"
+git push -u origin master
+
+git clone https://github.com/zzossig/hugo-theme-zzo.git
