4f1e42f9d85ad997d395be069d60c5fe511c4a77
1078503 <1078503@gmail.com>
2020-03-23 00:44:11 +0800

    修补

--- deploy.sh
+++ deploy.sh
@@ -6,7 +6,15 @@ git push origin master
 git pull origin master
 
 
+#################################### https://zhuanlan.zhihu.com/p/46973701
+export http_proxy=http://127.0.0.1:1081
+export https_proxy=http://127.0.0.1:1081
 
 git config --global http.https://github.com.proxy http://127.0.0.1:1081
 git config --global https.https://github.com.proxy http://127.0.0.1:1081
 
+export ALL_PROXY=socks5://127.0.0.1:1080
+
+unset http_proxy
+unset https_proxy
+####################################
