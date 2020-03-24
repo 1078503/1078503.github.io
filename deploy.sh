8703ec1da6e031c8dc0c2b8a75a86785767b5250
1078503 <1078503@gmail.com>
2020-03-25 04:47:18 +0800

    vscode

--- deploy.sh
+++ deploy.sh
@@ -29,5 +29,7 @@ git config --global user.email "1078503@gmail.com"
 
 ssh-keygen -t rsa -C "1078503@gmail.com"
 
+ssh -T git@github.com
+
 git rm -r --cached "themes"
 #####################################
